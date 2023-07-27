f7a9439e122f3759634e749f57c4340528012331
swiss0404 <swiss0404@gmail.com>
2023-07-28 03:58:31 +0700

    add landing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    backgroundImage: {
+      "gradient-grainy": "url('/img/gradient.jpeg')",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["cupcake"],
+  }
 } satisfies Config;
